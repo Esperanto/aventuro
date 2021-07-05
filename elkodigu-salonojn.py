d9cf4dcfdad4ad4bc4c7fd2517ef7328525f2c55
Neil Roberts <bpeeluk@yahoo.co.uk>
2021-07-05 23:17:02 +0200

    elkodigu-salonojn: Plibonigi la formaton

--- elkodigu-salonojn.py
+++ elkodigu-salonojn.py
@@ -11,18 +11,26 @@ def elkodigu_salonon(salono):
     p += 1
     nomo = elkodigu_ĉenon(salono[p : p + nomlongo])
     p += 20
-    ĉeno = salono[p]
-    p += 1
+    ĉeno = salono[p] | (salono[p + 1] << 8)
+    p += 2
 
-    rest = ' '.join('{:02x}'.format(x) for x in salono[p : GRANDECO_SALONO])
+    direktoj = []
+    for d in ["?", "n", "or", "s", "ok", "supren", "suben", "elen"]:
+        direktoj.append("{}: {:3d}".format(d, salono[p]))
+        p += 1
 
-    print("Nomo: {}, ĉeno: {}, rest: {}".format(nomo, ĉeno, rest))
+    return "nomo: {:20s}, ĉeno: {:3d}, {}".format(
+        nomo,
+        ĉeno,
+        ", ".join(direktoj))
 
 for fn in sys.argv[1:]:
     with open(fn, 'rb') as f:
         f.seek(0x101)
+        num = 1
         while True:
             salono = f.read(GRANDECO_SALONO)
             if salono[0] == 0:
                 break
-            elkodigu_salonon(salono)
+            print("{:3d}: {}".format(num, elkodigu_salonon(salono)))
+            num += 1
